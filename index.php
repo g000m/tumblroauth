e311eb03ea93fc7e78cf674e42a5732f5d124aaa
Jacob Budin <me@jacobbudin.com>
2011-04-10 23:05:34 -0400

    Added connection test to index.php

--- index.php
+++ index.php
@@ -19,31 +19,28 @@ $access_token = $_SESSION['access_token'];
 /* Create a TumblrOauth object with consumer/user tokens. */
 $connection = new TumblrOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
-/* If method is set change API call made. Test is called by default. */
-// $content = $connection->get('account/verify_credentials');
-
-$user = $connection->authenticate();
-$user = simplexml_load_string($user);
-
-$name = (string)$user->tumblelog['name'];
-
-$params = array('start' => 0,
-	'num' => 1);
-
-$read = $connection->read($name, $params);
-$read = simplexml_load_string($read);
-
-$url = (string)$read->posts->post[0]['url-with-slug'];
-
-echo '<strong>Connected via:</strong> ' . $name . '<br />';
-echo '<strong>Last post:</strong> <a href="' . $url . '">' . $url . '</a>';
-
-/* Some example calls */
-//$connection->get('users/show', array('screen_name' => 'abraham')));
-//$connection->post('statuses/update', array('status' => date(DATE_RFC822)));
-//$connection->post('statuses/destroy', array('id' => 5437877770));
-//$connection->post('friendships/create', array('id' => 9436992)));
-//$connection->post('friendships/destroy', array('id' => 9436992)));
-
 /* Include HTML to display on the page */
 include('html.inc');
+
+if(!isset($_GET['test'])){
+	echo 'Once you&#8217;ve linked an account to Tumblr, <a href="?test=1">test to see if it works</a>.';
+}
+else{
+	$user = $connection->authenticate();
+	$user = simplexml_load_string($user);
+
+	$name = (string)$user->tumblelog['name'];
+
+	$params = array('start' => 0,
+		'num' => 1);
+
+	$read = $connection->read($name, $params);
+	$read = simplexml_load_string($read);
+
+	$url = (string)$read->posts->post[0]['url-with-slug'];
+
+	if(!empty($name)){ echo '<strong>Connected. </strong><br />'; }
+	else{ echo '<strong>Failed to connect.</strong><br />'; }
+	echo '<strong>Connected via:</strong> ' . $name . '<br />';
+	echo '<strong>Last post:</strong> <a href="' . $url . '">' . $url . '</a>';
+}
